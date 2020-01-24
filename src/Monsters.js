88fe3760d1837e29317821ca906b9abc55dd5578
Negin <nuggetnegin@gmail.com>
2020-01-23 20:07:21 -0500

    mapping through monster info array and creating a new object of the values we want

--- src/Monsters.js
+++ src/Monsters.js
@@ -14,6 +14,21 @@ const Monsters = () =>{
             }
         )
     },[]); /*dependancy for useEffect - because its empty it will run only once per render*/
+
+    const getMonsterInfo = () =>{
+        let monsterInfo = monsterList.map(monster =>{
+            let monsterObject = {
+                name: monster.name,
+                species: monster.species,
+                element: monster.elements,
+                weakness: monster.weaknesses,
+                resistance: monster.resistances
+            }
+            return monsterObject;
+        })
+        return monsterInfo;
+    }
+
     console.log(monsterList)
 
     return (
