8ab25344c449db422c16d03d85fb4c42d17b5ca2
Negin <nuggetnegin@gmail.com>
2020-01-23 19:20:34 -0500

    initialized react app from scratch! using babel & webpack

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
+import ReactDOM from 'react-DOM'
 
 export default class App extends Component {
     render() {
         return (
             <div>
-                <h1>HELLOOO</h1>
+                <h1>hello</h1>
             </div>
         )
     }
 }
+ReactDOM.render(<App />, document.getElementById('root'));
