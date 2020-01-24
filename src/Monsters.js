a72d5c6989042a7b64d09c1c44326425439aa000
Negin <nuggetnegin@gmail.com>
2020-01-23 20:58:26 -0500

    displaying monster name and species.
    figuring out a way to display weakness and resistance array (PENDING brain tired)

--- src/Monsters.js
+++ src/Monsters.js
@@ -21,21 +21,54 @@ const Monsters = () =>{
                 name: monster.name,
                 species: monster.species,
                 element: monster.elements,
-                weakness: monster.weaknesses,
+                weakness: monster.weaknesses.map(weakness =>{
+                    element: weakness.element
+                }),
                 resistance: monster.resistances
             }
             return monsterObject;
         })
         return monsterInfo;
     }
-
+ 
     console.log(monsterList)
+    // const resistanceList = monsterInfoList.map((resistance) =>{
+    //     let resistanceObject = {
+    //         element: resistance.element,
+    //         stars: resistance.stars,
+    //         condition: resistance.condition
+    //     }
+    // })
+
+    const monsterInfoList = getMonsterInfo();
+    console.log(monsterInfoList);
+    // const resistanceList = resistanceList();
+
+    const listItem = monsterInfoList.map((monster) =>
+        <div class="monster">
+            <div className="name">{monster.name}</div>
+            <div className="species">{monster.species}</div>
+        </div>
+    )
+
+    // const weak = weaknessList.map((weak) =>
+    // <div class="weakness">
+    //     <div className="element">{weak.element}</div>
+    //     <div className="stars">{weak.stars}</div>
+    //     <div className="condition">{weak.condition}</div>
+    // </div>
+    // )
+
+    // console.log(monsterInfoList);
+    // console.log(listItem);
 
     return (
-
-
-        <div className="monsters">
-
+        
+        <div className="monstersContainer">
+            <div className="monsterContainer">
+                {listItem}
+                {/* {weak} */}
+            </div>
         </div>
     )
 }
